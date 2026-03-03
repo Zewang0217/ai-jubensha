9282a8fb2263b6b0f1c8f66b2457766675b16759
luobochuanqi <luobochuanqi@qq.com>
2026-03-03 22:37:16 +0800

    📝 docs(websocket): 更新设计文档并清理消息结构

--- ai-jubensha-backend/src/main/java/org/jubensha/aijubenshabackend/websocket/message/WebSocketMessage.java
+++ ai-jubensha-backend/src/main/java/org/jubensha/aijubenshabackend/websocket/message/WebSocketMessage.java
@@ -1,39 +1,33 @@
 package org.jubensha.aijubenshabackend.websocket.message;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 /**
- * 普通 WebSocket 消息
- *
- * @author zewang
- * @author luobo
+ * WebSocket 消息
  */
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class WebSocketMessage {
 
-    // 用于指定 GameRoom
-    private Long gameId;
-    private WebSocketMessageType type;
+    private String type;
     private Object payload;
 
-    public void setType(String phaseChange) {
-        this.type = WebSocketMessageType.valueOf(phaseChange);
+    public WebSocketMessage() {
     }
 
-    public enum WebSocketMessageType {
-        // 游戏状态更新
-        GAME_STATE_UPDATE,
-        // 聊天信息
+    public WebSocketMessage(String type, Object payload) {
+        this.type = type;
+        this.payload = payload;
+    }
+
+    public void setType(MessageType messageType) {
+        this.type = messageType.name();
+    }
+
+    public enum MessageType {
         CHAT_MESSAGE,
-        // 找到线索
-        CLUE_FOUND,
-        // 阶段更改
-        PHASE_CHANGE,
-        // 开始线索调查 payload: investigationScenes
-        START_INVESTIGATION
+        SCRIPT_READY,
+        START_INVESTIGATION,
+        PUBLIC_CLUE,
+        VOTE_RESULT
     }
 }
